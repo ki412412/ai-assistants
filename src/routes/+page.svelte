c1a4b0adf301123750d1f08357c4867de052961f
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-09-15 02:42:03 +0900

    chore: create basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="text-center">
+    <h1 class="text-3xl">English tutor</h1>
+</section>
+
+<section class="mt-12 w-full h-full">
+
+    <div class="flex gap-6 border rounded p-12 m-6 h-5/6">
+        <!-- original -->
+        <div class="self-stretch rounded overflow-hidden shadow-lg bg-white border w-full">
+            <textarea class="w-full h-full"></textarea>
+        </div>
+    
+        <!-- translate button -->
+        <div class="self-center place-self-center">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Translate
+            </button>
+        </div>
+        
+        <!-- result and comment -->
+        <div class="self-stretch rounded overflow-hidden shadow-lg bg-white border  w-full">
+            <textarea class="w-full h-full"></textarea>
+        </div>
+    </div>
+
+</section>
