907ed3e18b1b6e7eff5099a8b7802677e847851e
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-09-25 23:08:48 +0900

    feat: enable specifying tabs with URL anchor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,25 @@
 	import { Tabs, TabItem } from 'flowbite-svelte';
     import EnglishTutor from "$lib/components/pages/EnglishTutor.svelte";
     import GitCommitGenerator from "$lib/components/pages/GitCommitGenerator.svelte";
+    import { page } from '$app/stores';
+
+    // get anchor from url
+    let anchor = $page.url.href.split('#')[1];
+    // empty the anchor if it is not 'english-tutor' or 'git-commit-generator'
+    if (anchor !== 'english-tutor' && anchor !== 'git-commit-generator') {
+        anchor = '';
+    }
 </script>
 
 <Tabs class="flex justify-end">
-    <TabItem open title="English Tutor">
-        <EnglishTutor />
-    </TabItem>
-    <TabItem title="Git Commit Generator">
-        <GitCommitGenerator />
-    </TabItem>
+    <a href="#english-tutor">
+        <TabItem open={!anchor || anchor === 'english-tutor'} title="English Tutor">
+            <EnglishTutor />
+        </TabItem>
+    </a>
+    <a href="#git-commit-generator">
+        <TabItem open={anchor === 'git-commit-generator'} title="Git Commit Generator">
+            <GitCommitGenerator />
+        </TabItem>
+    </a>
 </Tabs>
