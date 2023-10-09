42d27ac60cb00b14c9e13e76838581d720392bd7
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-10-09 17:07:06 +0900

    refactor(Passkey): Improve registration verification function

--- src/lib/auth/passkey/index.ts
+++ src/lib/auth/passkey/index.ts
@@ -1,19 +1,51 @@
-import {
-    generateRegistrationOptions,
-    GenerateRegistrationOptionsOpts,
-} from '@simplewebauthn/server';
-import { isoBase64URL } from '@simplewebauthn/server/helpers';
 import { supabase } from "$lib/supabaseClient";
-import type { LoggedInUser } from './example-server';
+import {
+    // Registration
+    generateRegistrationOptions,
+    verifyRegistrationResponse,
+    // Authentication
+    generateAuthenticationOptions,
+    verifyAuthenticationResponse,
+} from '@simplewebauthn/server';
+import { isoBase64URL, isoUint8Array } from '@simplewebauthn/server/helpers';
+import type {
+    GenerateAuthenticationOptionsOpts,
+    GenerateRegistrationOptionsOpts,
+    VerifiedAuthenticationResponse,
+    VerifiedRegistrationResponse,
+    VerifyAuthenticationResponseOpts,
+    VerifyRegistrationResponseOpts,
+} from '@simplewebauthn/server';
+import type {
+    AuthenticationResponseJSON,
+    AuthenticatorDevice,
+    RegistrationResponseJSON,
+} from '@simplewebauthn/typescript-types';
+import type { LoggedInUser } from './example-server.ts';
 
 export class Passkey {
 
     // Human-readable title for your website
-    private rpName: string = 'SimpleWebAuthn Example';
+    private rpName: string;
     // A unique identifier for your website
-    private rpID: string = 'localhost';
+    private rpID: string;
     // The URL at which registrations and authentications should occur
-    private origin: string = `https://${this.rpID}`;
+    private origin: string;
+
+    // contructor
+    constructor() {
+        // TODO: Read from environment variables instead
+        this.rpName = 'SimpleWebAuthn Example';
+        this.rpID = 'localhost';
+        this.origin = `https://${this.rpID}`;
+
+        if (process.env.ENABLE_HTTPS) {
+            this.origin = `https://${this.rpID}`;
+        } else {
+            const port = 5173;
+            this.origin = `http://localhost:${port}`;
+        }
+    }
 
     public async getUserFromDB(loggedInUserId: string): Promise<LoggedInUser> {
         const { data, error } = await supabase.from("users")
@@ -77,5 +109,63 @@ export class Passkey {
             supportedAlgorithmIDs: [-7, -257],
         };
         return await generateRegistrationOptions(opts);
+    }
+
+    public async verifyRegistrationResponse(body: RegistrationResponseJSON, expectedChallenge: string) {
+        const opts: VerifyRegistrationResponseOpts = {
+            response: body,
+            expectedChallenge: `${expectedChallenge}`,
+            expectedOrigin: this.origin,
+            expectedRPID: this.rpID,
+            requireUserVerification: true,
+        };
+        return await verifyRegistrationResponse(opts);
+    }
+
+    public async getRegistrationResponse(request: Request): Promise<RegistrationResponseJSON> {
+        const jsonData = await request.json();
+        return {
+            id: jsonData.id,
+            rawId: jsonData.rawId,
+            response: jsonData.response,
+            authenticatorAttachment: jsonData.authenticatorAttachment,
+            clientExtensionResults: jsonData.clientExtensionResults,
+            type: jsonData.type,
+        } as RegistrationResponseJSON;
+    }
+
+    private async doesDeviceExist(user: LoggedInUser, credentialID: Uint8Array): Promise<AuthenticatorDevice | undefined> {
+        return user.devices.find((device) => {
+            return isoUint8Array.areEqual(device.credentialID, credentialID);
+        });
+    }
+
+    public async registerNewDeviceToDBIfNotExists(user: LoggedInUser, verification: VerifiedRegistrationResponse, registrationResponse: RegistrationResponseJSON): Promise<void> {
+        const { verified, registrationInfo } = verification;
+        if (!verified || !registrationInfo) {
+            throw new Error('Registration verification failed.');
+        }
+        const { credentialPublicKey, credentialID, counter, credentialDeviceType, credentialBackedUp } = registrationInfo;
+        
+        // Register the new device to the user's account if it doesn't already exist
+        const existingDevice = await this.doesDeviceExist(user, credentialID);
+        if (!existingDevice) {
+
+            const {data, error } = await supabase.from('authenticators').insert([
+                {
+                    user_id: user.id,
+                    credentialID: isoBase64URL.fromBuffer(credentialID),
+                    credentialPublicKey: isoBase64URL.fromBuffer(credentialPublicKey),
+                    counter: counter,
+                    transports: registrationResponse.response.transports,
+                    credentialDeviceType: credentialDeviceType,
+                    credentialBackedUp: credentialBackedUp,
+                }
+            ]);
+
+            if (error) {
+                throw error;
+            }
+        }
     }
 }
